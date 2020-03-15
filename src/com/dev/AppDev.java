d57b95e3f8d61cc6cc02093323f0978b3fb60a77
dc480506 <dev.c@somaiya.edu>
2020-03-15 13:52:34 +0530

    Java Swing project complete

---
+++ src/com/dev/AppDev.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dev;
+
+/**
+ *
+ * @author dev
+ */
+public class AppDev {
+    public static void main(String args[]){
+       try{
+        new Splash();
+    }catch(Exception e){
+        
+    }
+    }
+}
